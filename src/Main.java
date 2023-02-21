ac7d6c13f6911dc1154704a4a89c279f6369e029
Sigmaalex <sigaalex@mail.ru>
2023-02-21 16:01:38 +0500

    начало

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
-public class Main {
+}public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+//        task1();
+//        task2();
+//        task3();
+//        task4();
+//        task5();
+
